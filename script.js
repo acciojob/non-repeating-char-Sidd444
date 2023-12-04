17f3af5ef5d85ac393644e5f5680b602f21a16c0
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-12-04 20:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const m=new Map();
+	for(let i=0;i<str.length;i++){
+		if(m.has(str[i])) m.set(str[i],m.get(str[i]+1));
+		else m.set(str[i],1);
+	}
+	for(let i=0;i<str.length;i++) if(m.get(str[i])==1) return str[i];
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
